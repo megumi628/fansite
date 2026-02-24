5c4268927527683b32da813a1e3be5d32a2c9f55
Mincy <mincyxue@126.com>
2026-02-23 21:06:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+  let title = document.querySelector("h1") || document.getElementById("mainTitle");
+  
+  if (title) {
+    title.style.transition = "color 0.5s ease";
+    
+    setInterval(function() {
+      let r = Math.floor(Math.random() * 128) + 128;
+      let g = Math.floor(Math.random() * 128) + 128;
+      let b = Math.floor(Math.random() * 128) + 128;
+      title.style.color = "rgb(" + r + "," + g + "," + b + ")";
+    }, 1000);
+  
+  let secondPics = [
+    "tate2.JPG",
+	"tate8.JPG",
+	"tate7.JPG", 
+  ];
+
+  let secondImg = document.querySelector("#mainImage");
+  
+  if (secondImg) {
+    let randomIndex2 = Math.floor(Math.random() * secondPics.length);
+    secondImg.src = secondPics[randomIndex2];
+    
+    setInterval(function() {
+      let randomIndex = Math.floor(Math.random() * secondPics.length);
+      secondImg.src = secondPics[randomIndex];
+    }, 2000);
+  } 
+  }
+};
